4250e0ae00565a3eba106fc0b7f2ea5ae9545cd0
micsamuel <mykelsown@gmail.com>
2026-03-17 13:11:10 +0000

    x

--- MethodsAndTesting/file-handling.go
+++ MethodsAndTesting/file-handling.go
@@ -7,7 +7,7 @@ import (
 
 func FileHandler() ([]byte, bool) {
 	// Read banner file
-	data, err := os.ReadFile("../banners/standard.txt")
+	data, err := os.ReadFile("banners/standard.txt")
 	if err != nil {
 		fmt.Println("Error")
 		return []byte{}, false
